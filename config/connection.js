d4b5054844589537d64241d23932a8fbc5c8616e
lhutchinson22 <lbhutchinson022@gmail.com>
2021-01-12 18:30:09 -0800

    updated jaws 3

--- config/connection.js
+++ config/connection.js
@@ -17,6 +17,7 @@ connection.connect((err) => {
     console.error(`error connecting: ${err.stack}`);
     return;
   }
+  console.log("connection");
   console.log(`connected as id ${connection.threadId}`);
 });
 
